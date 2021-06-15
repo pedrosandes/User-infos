d1b7c75caf20f524e44d935fd402a9d4becbd9ae
Pedro Sandes <pedrosandes122@gmail.com>
2021-06-14 22:27:55 -0300

    Projeto finalizado

--- js/main.js
+++ js/main.js
@@ -7,24 +7,24 @@ fetch(URL) // Faz uma requisição.
   .then(function (json) {
     createContainer();
     json.data.forEach(({ avatar, first_name, last_name, email, id }) => {
-      user(avatar, first_name, last_name);
+      createUser(avatar, first_name, last_name, email, id);
     });
   })
   .catch(function (err) {
     console.log('Aconteceu um ' + err);
   });
 
-/* User List */
+/* -------------- User list ---------------*/
 // Cria o elemento "div" com meu "container".
 function createContainer() {
   let divElem = document.createElement('div');
   divElem.setAttribute('class', 'container');
   document.body.appendChild(divElem);
-  divElem.appendChild(title());
-  divElem.appendChild(userList());
+  divElem.appendChild(createTitle());
+  divElem.appendChild(createUserList());
 }
 // Cria o elemento "h1".
-function title() {
+function createTitle() {
   let titleElem = document.createElement('h1');
   titleElem.setAttribute('class', 'title');
   titleElem.textContent = 'User List';
@@ -32,30 +32,29 @@ function title() {
 }
 
 // Cria meu elemento "Ul".
-function userList() {
+function createUserList() {
   let ulElem = document.createElement('ul');
   ulElem.setAttribute('class', 'list');
   return ulElem;
 }
 
 // Cria o elemento "Li".
-function user(img, firstName, lastName) {
+function createUser(img, firstName, lastName, email, id) {
   let containerUl = document.querySelector('ul');
 
   let liElem = document.createElement('li');
 
   liElem.setAttribute('class', 'list__item');
   containerUl.appendChild(liElem);
-  liElem.appendChild(userImg(img));
-  liElem.appendChild(userName(firstName, lastName));
+  liElem.appendChild(createUserImg(img));
+  liElem.appendChild(createUserName(firstName, lastName));
 
   // Ao clicar no li, adiciona o modal
   liElem.addEventListener('click', function createModal() {
     let modalElem = document.createElement('div');
     modalElem.setAttribute('class', 'modal');
     document.body.appendChild(modalElem);
-    modalElem.appendChild(userInfo());
-    modalElem.appendChild(createUserModalImg(img));
+    modalElem.appendChild(createUserInfo(img, firstName, lastName, email, id));
     // Seleciono o botão "close".
     let close = document.querySelector('.close');
     // Remove o modal da tela.
@@ -65,37 +64,40 @@ function user(img, firstName, lastName) {
   });
 }
 
-// Cria o elemento "Img".
-function userImg(avatarLink) {
+// Cria o elemento "Img", com o imagem do avatar da pessoa
+function createUserImg(avatarLink) {
   let imgElem = document.createElement('img');
   imgElem.setAttribute('src', avatarLink);
   imgElem.setAttribute('class', 'list__img');
   return imgElem;
 }
 
-// Cria o elemento "p".
-function userName(firstName, lastName) {
+// Cria o elemento "p", com o nome da pessoa.
+function createUserName(firstName, lastName) {
   let pElem = document.createElement('p');
   pElem.setAttribute('class', 'list__text');
   pElem.textContent = `${firstName} ${lastName}`;
   return pElem;
 }
 
-/* MODAL */
-// Cria uma "div", com meu UserInfo
-function userInfo() {
+/* -------------- MODAL ---------------*/
+// Cria um elemento "div", com meu UserInfo
+function createUserInfo(img, firstName, lastName, email, id) {
   let infoElem = document.createElement('div');
   infoElem.setAttribute('class', 'user');
-  infoElem.appendChild(close());
+  infoElem.appendChild(createCloseButton());
+  infoElem.appendChild(createUserModalImg(img));
+  infoElem.appendChild(createBoxText(firstName, lastName, email, id));
   return infoElem;
 }
-
-function close() {
+// Cria um elemento "i", que é um botão.
+function createCloseButton() {
   let closeElem = document.createElement('i');
   closeElem.setAttribute('class', 'close');
   return closeElem;
 }
 
+// Cria um elemento "img", com a imagem do avatar da pessoa,
 function createUserModalImg(imgLink) {
   let modalImg = document.createElement('img');
   modalImg.setAttribute('class', 'user__modalImg');
@@ -103,9 +105,36 @@ function createUserModalImg(imgLink) {
   modalImg.setAttribute('alt', 'Avatar image');
   return modalImg;
 }
+// Cria um elemento "div", com os textos dentro.
+function createBoxText(firstName, lastName, email, id) {
+  let boxTextElem = document.createElement('div');
+  boxTextElem.setAttribute('class', 'user__boxText');
+  boxTextElem.appendChild(createUserModalFullName(firstName, lastName));
+  boxTextElem.appendChild(createUserModalEmail(email));
+  boxTextElem.appendChild(createUserModalID(id));
+  return boxTextElem;
+}
 
-// function userHeader() {
-//   let headerElem = document.createElement('div');
-//   headerElem.setAttribute('class', 'user__header');
-//   return headerElem;
-// }
+// Cria um elemento "p" com o nome completo.
+function createUserModalFullName(firstName, lastName) {
+  let pElem = document.createElement('p');
+  pElem.setAttribute('class', 'user__modalText');
+  pElem.textContent = `Nome: ${firstName} ${lastName}`;
+  return pElem;
+}
+
+// Cria um elemento "p", com o ID da pessoa.
+function createUserModalID(id) {
+  let pElem = document.createElement('p');
+  pElem.setAttribute('class', 'user__modalText');
+  pElem.textContent = `ID: ${id}`;
+  return pElem;
+}
+
+// Cria um elemento "p", com o Email da pessoa.
+function createUserModalEmail(email) {
+  let pElem = document.createElement('p');
+  pElem.setAttribute('class', 'user__modalText');
+  pElem.textContent = `Email: ${email}`;
+  return pElem;
+}
